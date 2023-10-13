040a93898ae16044edcb487ac7b653b85279e928
TRESAS777 <103174712+TRESAS777@users.noreply.github.com>
2023-10-13 13:12:58 -0500

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,5 @@
+moneda_extranjera=float(input("ingrese el valor actual de la moneda que desea convertir: "))
+cuanto=float(input("ingrese cuanto desea convertir: "))
+
+moneda_local=moneda_extranjera*cuanto
+print(moneda_local)
